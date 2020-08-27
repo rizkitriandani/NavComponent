14da819e2cbef54b63ad00961ca4722fc3cf550d
rizkitriandani <rizkitriandani@yahoo.co.id>
2020-08-27 13:27:44 +0700

    Menambah Destination.

---
+++ app/src/main/java/com/belajar/navcomponent/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.belajar.navcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.belajar.navcomponent.databinding.FragmentHomeBinding
+import java.util.zip.Inflater
+
+
+class HomeFragment : Fragment() {
+
+
+    private lateinit var binding : FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+
+}
